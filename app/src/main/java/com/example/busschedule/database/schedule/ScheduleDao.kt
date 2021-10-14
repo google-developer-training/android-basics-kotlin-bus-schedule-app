93bb3f0455d6eaa7f6ed8e2c7660ae97845e12f1
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-14 11:26:01 +0200

    Added Flow

--- app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
+++ app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
@@ -2,12 +2,13 @@ package com.example.busschedule.database.schedule
 
 import androidx.room.Dao
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ScheduleDao {
     @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
-    fun getAll(): List<Schedule>
+    fun getAll(): Flow<List<Schedule>>
 
     @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
-    fun getByStopName(stopName: String): List<Schedule>
+    fun getByStopName(stopName: String): Flow<List<Schedule>>
 }
--- app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -4,11 +4,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDao
+import kotlinx.coroutines.flow.Flow
 import java.lang.IllegalArgumentException
 
 class BusScheduleViewModel(private val scheduleDao: ScheduleDao) : ViewModel() {
-    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
-    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+    fun fullSchedule(): Flow<List<Schedule>> = scheduleDao.getAll()
+    fun scheduleForStopName(name: String): Flow<List<Schedule>> = scheduleDao.getByStopName(name)
 }
 
 class BusScheduleViewModelFactory(
