b43d7d24f76866fef414492f7bfce746bde39c8d
Maryna Hryshchenko <mari.gryshchenko54@gmail.com>
2023-09-11 14:09:12 +0300

    finish

---
+++ app/src/main/java/com/example/busschedule/BusStopAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.busschedule
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.busschedule.database.schedule.Schedule
+import com.example.busschedule.databinding.BusStopItemBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+
+class BusStopAdapter(private val onItemClicked: (Schedule) -> Unit) : ListAdapter<Schedule, BusStopAdapter.BusStopViewHolder>(DiffCallback) {
+
+    class BusStopViewHolder(private var binding: BusStopItemBinding): RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SimpleDateFormat")
+        fun bind(schedule: Schedule) {
+            binding.stopNameTextView.text = schedule.stopName
+            binding.arrivalTimeTextView.text = SimpleDateFormat(
+                "h:mm a").format(
+                Date(schedule.arrivalTime.toLong() * 1000)
+            )
+        }
+    }
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BusStopAdapter.BusStopViewHolder {
+        val viewHolder = BusStopViewHolder(
+            BusStopItemBinding.inflate(
+                LayoutInflater.from( parent.context),
+                parent,
+                false
+            )
+        )
+        viewHolder.itemView.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            onItemClicked(getItem(position))
+        }
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: BusStopAdapter.BusStopViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<Schedule>() {
+            override fun areItemsTheSame(oldItem: Schedule, newItem: Schedule): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Schedule, newItem: Schedule): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/busschedule/database/AppDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.busschedule.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.busschedule.database.schedule.Schedule
+import com.example.busschedule.database.schedule.ScheduleDao
+
+@Database(entities = arrayOf(Schedule::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun scheduleDao(): ScheduleDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "app_database")
+                    .createFromAsset("database/bus_schedule.db")
+                    .build()
+                INSTANCE = instance
+
+                instance
+            }
+        }
+    }
+}
